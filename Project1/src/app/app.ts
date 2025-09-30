d044e3295c43c81ccc4ccb4a27782f0f2b1bf900
Samu-Mind <samuelayllonsevilla1@gmail.com>
2025-09-30 17:28:07 +0200

    Exercise-1

--- Project1/src/app/app.ts
+++ Project1/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Question } from '../components/question/question';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, Question],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
